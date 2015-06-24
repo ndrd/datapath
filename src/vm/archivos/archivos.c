4a8308d95a5aae20b1e64105a4ebfd69c14221cf
Jonathan de Jesús Andrade López <ndrd@ciencias.unam.mx>
2015-06-24 12:43:48 -0500

    moviendo cosas

---
+++ src/vm/archivos/archivos.c
@@ -0,0 +1,8 @@
+#ifndef ARCHIVOS_INCLUDE
+#define ARCHIVOS_INCLUDE
+
+/*
+* Leemos un archivo dado, y usando el compilador que se
+* incluyo en el proyecto, lo volveremos binario, para despues
+* leerlo, y hacer el fetch de las instrucciones.
+*/
